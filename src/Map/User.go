1df86c43b42aaa7b0da1415eddac8777b1357974
MIAbon <werewolf2101@msn.com>
2020-06-03 16:00:49 +0800

    learn12 设置值、链式调用

--- src/Map/User.go
+++ src/Map/User.go
@@ -1,7 +1,15 @@
 package Map
 
-type User map[string]string
+type User map[string]interface{}
 
 func NewUser() User {
-	return make(map[string]string)
+	return make(map[string]interface{})
+}
+
+//func (this User) With(k string,v string) {
+//	this[k]=v
+//}
+func (this User) With(k string, v interface{}) User {
+	this[k] = v
+	return this
 }
--- src/main.go
+++ src/main.go
@@ -11,14 +11,21 @@ import (
 //}
 
 //map chan slice
-func change(u Map.User) {
-	u["id"] = "503"
-}
+//func change(u Map.User) {
+//	u["id"] = "503"
+//}
 
 func main() {
 	u := Map.NewUser()
-	u["id"] = "100"
-	u["name"] = "MIAbon"
-	change(u)
+	//u["id"] = "100"
+	//u["name"] = "MIAbon"
+	//change(u)
+	//fmt.Println(u)
+
+	//u.With("id", "100")
+	//u.With("name", "MIAbon")
+
+	u.With("id", 100).
+		With("name", "MIAbon")
 	fmt.Println(u)
 }
