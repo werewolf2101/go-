c5879123f2978b16f59ade4c185d50b6039eb4fa
MIAbon <werewolf2101@msn.com>
2020-06-03 12:14:18 +0800

    learn3 自己的String(3):遍历字符串

--- src/String/String.go
+++ src/String/String.go
@@ -5,7 +5,7 @@ import (
 )
 
 type String string //我自己的 String
-func (this String) Len() int{
+func (this String) Len() int {
 	return len(this)
 }
 
@@ -13,7 +13,13 @@ func From(str string) String {
 	return String(str)
 }
 
+func (this String) Each(f func(item string)) {
+	for i := 0; i < len(this); i++ {
+		f(fmt.Sprintf("%c", this[i]))
+	}
+}
+
 func FromInt(n int) String {
 	//return String(strconv.Itoa(n)) //相同
-	return String(fmt.Sprintf("%d",n))
+	return String(fmt.Sprintf("%d", n))
 }
--- src/main.go
+++ src/main.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 )
 
-func main()  {
-	s:=String.From("abc")
-	fmt.Println(s,s.Len())
+func main() {
+	s := String.From("abc")
+	fmt.Println(s, s.Len())
 
-	ss:=String.FromInt(123)
+	//for i:=0; i < len(s); i++{
+	//	fmt.Println(i, " ", fmt.Sprintf("%c", s[i])) //不转换打印出的是阿斯克码
+	//}
+
+	s.Each(func(item string) {
+		fmt.Println(item)
+	})
+
+	ss := String.FromInt(123)
 	fmt.Println(ss)
 }
