230c320264781866187a1fb881f2ef0028112fbf
MIAbon <werewolf2101@msn.com>
2020-06-03 14:49:16 +0800

    learn10 构造函数技巧(3):进一步提升逼格

--- src/Object/User.go
+++ src/Object/User.go
@@ -7,10 +7,14 @@ type User struct {
 }
 
 //有选择性的对ID进行赋值 //代码量增加但是调用方便
-func NewUser(fs ...func(u *User)) *User { //...可变参数
+func NewUser(fs ...UserAttrFunc) *User { //...可变参数
 	u := new(User)
-	for _, f := range fs {
-		f(u)
-	}
+	//for _, f := range fs {
+	//	//f(u)
+	//	f.apply(u)
+	UserAttrFuncs(fs).apply(u)
+	//}
+	//打印类型
+	//fmt.Printf("%T\n", fs)
 	return u
 }
--- src/Object/UserAttrs.go
+++ src/Object/UserAttrs.go
@@ -1,12 +1,23 @@
 package Object
 
+//参数可省略
+//type UserAttrFunc func(u *User)
+type UserAttrFunc func(*User) //设置User属性的函数类型
+type UserAttrFuncs []UserAttrFunc
+
+func (this UserAttrFuncs) apply(u *User) {
+	for _, f := range this {
+		f(u)
+	}
+}
+
 //只返回func本身不返回结果
-func WithUserID(id int) func(u *User) {
+func WithUserID(id int) UserAttrFunc {
 	return func(u *User) {
 		u.Id = id
 	}
 }
-func WithUserName(name string) func(u *User) {
+func WithUserName(name string) UserAttrFunc {
 	return func(u *User) {
 		u.Name = name
 	}
